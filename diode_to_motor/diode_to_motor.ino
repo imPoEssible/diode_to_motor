619d0ae5d5101371e5c0378edc96fe1e6fa66f13
TJ Kim <taejin.kim96@gmail.com>
2015-11-05 22:53:35 -0500

    Controlling 4 motors at once for Sprint Review 2

--- diode_to_motor/diode_to_motor.ino
+++ diode_to_motor/diode_to_motor.ino
@@ -5,25 +5,42 @@
 // set up motor
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *myMotor3 = AFMS.getMotor(3);
+Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
 
-int wow;
+byte motorSpeed1;
+byte motorSpeed2;
+byte motorSpeed3;
+byte motorSpeed4;
+
 void setup() {
   Serial.begin(9600);
 
   // Initialize Motor
   AFMS.begin();
-  myMotor1->run(FORWARD);
   myMotor1->setSpeed(10);
-  
+  myMotor2->setSpeed(10);
+  myMotor3->setSpeed(10);
+  myMotor4->setSpeed(10);
+  myMotor1->run(FORWARD);
+  myMotor2->run(FORWARD);
+  myMotor3->run(FORWARD);
+  myMotor4->run(FORWARD);
 }
 
 void loop() {
   delay(10);
-  wow = (analogRead(0)-530)/5 +30;
-  Serial.print(analogRead(0));
-  Serial.print(",");
-  Serial.println(wow);
-  myMotor1->setSpeed(wow);
-
+  motorSpeed1 = map(analogRead(0), 530, 860, 10, 100);
+  motorSpeed2 = 1.1 * motorSpeed1;
+  motorSpeed3 = 1.25 * motorSpeed1;
+  motorSpeed4 = 1.5 * motorSpeed1;
+//  Serial.print(analogRead(0));
+//  Serial.print(",");
+//  Serial.println(motorSpeed);
+  myMotor1->setSpeed(motorSpeed1);
+  myMotor2->setSpeed(motorSpeed2);
+  myMotor3->setSpeed(motorSpeed3);
+  myMotor4->setSpeed(motorSpeed4);
 }
 
